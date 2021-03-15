c8d190349060781e83c190ba7713979eea2c26e9
Marcelo Farias <hmfarias7@gmail.com>
2021-03-15 20:22:53 -0300

    Hasta la barra de search

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 // FOR BURGER MENU IN MOBILE MODE -------------
 let navIcon = document.getElementById("navIcon"); //burger or X image
 let navMenu = document.getElementById("navMenu"); //options menu
+let searchIcon = document.getElementById("searchIcon"); // search icon
 let navIconImageClose = "";
 let navIconImageBurger = "";
+let searchIconImage = "";
+
 
 navIcon.addEventListener("click", () => {
     changeIconBurger();
@@ -22,26 +25,31 @@ function changeIconBurger() {
 //THEMES ------------------------------------------------
 let logo = document.getElementById("logo"); //para poder cambiar al logo modo light o dark
 
-// function to set a given theme/color-scheme
+// function to set a given theme/color-scheme and icons update
 function setTheme(themeName) {
     localStorage.setItem("theme", themeName);
     document.documentElement.className = themeName;
 
-    // for the icons and principal logo
+    // for the icons, principal logo and search icon
     if (themeName === "theme-light") {
         logo.src = "./images/logo-mobile.svg"; //principal logo light mode
-        navIconImageClose = "./images/close.svg"; // X logo in light mode
-        navIconImageBurger = "./images/burger.svg"; // burger logo in light mode
+        navIconImageClose = "./images/close.svg"; // X icon in light mode
+        navIconImageBurger = "./images/burger.svg"; // burger icon in light mode
+        searchIconImage = "./images/icon-search.svg"; //search icon in light mode
     } else {
         logo.src = "./images/logo-mobile-modo-noct.svg"; //principal logo dark mode
-        navIconImageClose = "./images/close-modo-noct.svg"; // X logo in dark mode
-        navIconImageBurger = "./images/burger-modo-noct.svg"; // burger logo in dark mode
+        navIconImageClose = "./images/close-modo-noct.svg"; // X icon in dark mode
+        navIconImageBurger = "./images/burger-modo-noct.svg"; // burger icon in dark mode
+        searchIconImage = "./images/icon-search-modo-noct.svg"; // search icon in dark mode
     }
 
     //update the burger icon
     navMenu.style.display === "none" || navMenu.style.display === ""
         ? (navIcon.src = navIconImageBurger)
         : (navIcon.src = navIconImageClose);
+    
+    //update the search icon
+    searchIcon.src = searchIconImage;
 }
 
 // function to toggle between light and dark theme
