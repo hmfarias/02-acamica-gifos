d49e39889922e4b65130a419a816a14cceeb0886
Marcelo Farias <hmfarias7@gmail.com>
2021-03-15 13:21:57 -0300

    Modo nocturno a nivel menú hamburguesa

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,71 @@
 // FOR BURGER MENU IN MOBILE MODE -------------
-let navIcon = document.getElementById('navIcon');
-let navMenu = document.getElementById('navMenu');
+let navIcon = document.getElementById("navIcon"); //burger or X image
+let navMenu = document.getElementById("navMenu"); //options menu
+let navIconImageClose = "";
+let navIconImageBurger = "";
 
-navIcon.addEventListener(('click'),() => {
-    if(navMenu.style.display === 'none') {
-        navIcon.src = './images/close.svg';
-        navMenu.style.display = 'block';
-    } else {
-        navIcon.src ='./images/burger.svg';
-        navMenu.style.display = 'none';
-    }
+navIcon.addEventListener("click", () => {
+    changeIconBurger();
 });
+
+function changeIconBurger() {
+    if (navMenu.style.display === "none") {
+        navIcon.src = navIconImageClose;
+        navMenu.style.display = "block";
+    } else {
+        navIcon.src = navIconImageBurger;
+        navMenu.style.display = "none";
+    }
+}
 // END FOR BURGER MENU IN MOBILE MODE ---------
+
+//THEMES ------------------------------------------------
+let logo = document.getElementById("logo"); //para poder cambiar al logo modo light o dark
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+
+    // for the icons and principal logo
+    if (themeName === "theme-light") {
+        logo.src = "./images/logo-mobile.svg"; //principal logo light mode
+        navIconImageClose = "./images/close.svg"; // X logo in light mode
+        navIconImageBurger = "./images/burger.svg"; // burger logo in light mode
+    } else {
+        logo.src = "./images/logo-mobile-modo-noct.svg"; //principal logo dark mode
+        navIconImageClose = "./images/close-modo-noct.svg"; // X logo in dark mode
+        navIconImageBurger = "./images/burger-modo-noct.svg"; // burger logo in dark mode
+    }
+
+    //update the burger icon
+    navMenu.style.display === "none" || navMenu.style.display === ""
+        ? (navIcon.src = navIconImageBurger)
+        : (navIcon.src = navIconImageClose);
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        setTheme("theme-light");
+    } else {
+        setTheme("theme-dark");
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        setTheme("theme-dark");
+    } else {
+        setTheme("theme-light");
+    }
+})();
+
+// nocturn mode menu option
+let changeMode = document.getElementById("changeMode");
+changeMode.addEventListener("click", () => {
+    toggleTheme();
+});
+
+// END THEMES ------------------------------------------------
